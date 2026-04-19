11955b3caaf1b8090be8d100cdec0c25dc2b878e
CookieCutter07 <dustinboston22@gmail.com>
2026-04-19 01:04:59 -0500

    fix(theme): use CardThemeData for cardTheme

--- lib/main.dart
+++ lib/main.dart
@@ -76,7 +76,7 @@ class App extends StatelessWidget {
             shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
           ),
         ),
-        cardTheme: CardTheme(
+        cardTheme: CardThemeData(
           elevation: 0,
           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(22)),
         ),
